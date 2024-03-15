3db1b5d6e232a924e667b45ac1da523274684fb0
ECV96 <cantuvemanuel@gmail.com>
2024-03-14 18:46:52 -0600

    Swagger documentation added

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,8 +4,48 @@ import { validateCreate, validateLogin } from "../validators/auth.js"
 
 const router = express.Router()
 
+/**
+ * Post Register
+ * @openapi
+ * /register:
+ *  post:
+ *      tags:
+ *          - Auth
+ *      summary: "User Registration"
+ *      description: "Endpoint to Register a new user in the database"
+ *      requestBody:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/userRegistration"
+ *      responses:
+ *          "200":
+ *              description: "Success Login"
+ */
 router.post('/register', validateCreate, register)
 
+/**
+ * Post Login
+ * @openapi
+ * /login:
+ *  post:
+ *      tags:
+ *          - Auth
+ *      summary: "User Login"
+ *      description: "Endpoint to login a user and generate JWT"
+ *      requestBody:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/userLogin"
+ *      responses:
+ *          "200":
+ *              description: "Success Login"
+ *          "404":
+ *              description: "User not found"
+ *          "409":
+ *              description: "Invalid credentials"
+ */
 router.post('/login', validateLogin, login)
 
 export default router
--- app/routes/users.js
+++ app/routes/users.js
@@ -5,8 +5,43 @@ import { checkAuth } from "../middlewares/auth.js"
 
 const router = express.Router()
 
+/**
+ * Get /
+ * @openapi
+ * /:
+ *  get:
+ *      tags:
+ *          - Users
+ *      summary: "Get a list of all user"
+ *      description: "Get a list of all user"
+ *      responses:
+ *          "200":
+ *              description: "OK"
+ */
 router.get('/', checkAuth, getUsers)
 
+/**
+ * Get /:id
+ * @openapi
+ * /:id:
+ *  get:
+ *      tags:
+ *          - Users
+ *      summary: "Get a user"
+ *      description: "Get a specific user from an id"
+ *      parameters:
+ *          - name: "id"
+ *            in: "path"
+ *            description: "User Id"
+ *            required: "true"
+ *            schema:
+ *                type: "string"
+ *      responses:
+ *          "200":
+ *              description: "OK"
+ *          "404":
+ *              description: "User not found"
+ */
 router.get('/:id', validateGetById, checkAuth, getUser)
 
 export default router
