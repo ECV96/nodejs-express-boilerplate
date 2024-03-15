ec1dbe4f6c8732d64eb68085f09416a747bfa915
ECV96 <cantuvemanuel@gmail.com>
2024-03-14 18:34:47 -0600

    Token authorization added to user endpoints

---
+++ app/middlewares/auth.js
@@ -0,0 +1,20 @@
+import { verifyToken } from "../helpers/generateToken.js"
+
+//Verifies token 
+const checkAuth = async (req,res,next) => {
+    try {
+        const token = req.headers.authorization?.split(' ').pop() || null
+        const tokenData = await verifyToken(token)
+
+        if(tokenData?._id) {
+            next()
+        } else {
+            return res.status(409).send({error: "Not Authorized"})
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+export { checkAuth }
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,11 +1,12 @@
 import express from "express"
 import {getUsers, getUser} from "../controllers/users.js"
 import { validateGetById } from "../validators/users.js"
+import { checkAuth } from "../middlewares/auth.js"
 
 const router = express.Router()
 
-router.get('/', getUsers)
+router.get('/', checkAuth, getUsers)
 
-router.get('/:id', validateGetById, getUser)
+router.get('/:id', validateGetById, checkAuth, getUser)
 
 export default router
